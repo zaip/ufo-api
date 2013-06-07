196dbbdb5a8d6d6a1744c0eb415d0d2630cd3533
Alex Piaz <alex@piaz.com.br>
2013-06-06 21:30:12 -0300

    Criando mais alguns testes

--- tests/specs/ufoapi_spec.js
+++ tests/specs/ufoapi_spec.js
@@ -4,8 +4,8 @@
  */
 var frisby   = require('/usr/lib/node_modules/frisby/lib/frisby.js');
 var protocol = 'http://'
-var domain   = 'api.piaz.com.br';
-var auth_url = protocol+'alex:2122'+'@localhost/auth';
+var domain   = 'ufo-api.zaip.net';
+//var auth_url = protocol+'alex:2122'+'@localhost/auth';
 var url      = protocol+domain;
 
 frisby.globalSetup({ // globalSetup is for ALL requests
@@ -14,7 +14,32 @@ frisby.globalSetup({ // globalSetup is for ALL requests
   }
 });
 
-frisby.create('Inspecionando o corpo da mensagem http')
-  .get('http://asciime.heroku.com/generate_ascii?s=THE TRUTH IS OUT THERE')
-  .inspectBody()
-.toss()
+// frisby.create('Inspecionando o corpo da mensagem http')
+//   .get('http://asciime.heroku.com/generate_ascii?s=THE TRUTH IS OUT THERE')
+//   .inspectBody()
+// .toss()
+
+frisby.create('Garantir que a resposta solicitada em JSON está mesmo em JSON')
+	.get(url + '/v1/casos/1')
+	.expectStatus(200)
+	.expectHeaderContains('content-type', 'application/json')
+	.expectJSONTypes('_links', {
+		self: Object
+	})
+	.expectJSONTypes('prev', {
+		href: function(val) { expect(val).toBeTypeOrNull(String); }
+	})
+	.expectJSONTypes('_embedded', {
+		id: Number,
+		nome: String,
+		descricao: String,
+		ano: Number,
+		pais: String
+
+	})
+	.toss()
+
+frisby.create('Garantir que uma requisição POST não é aceita pela API')
+	.post(url + '/v1/casos/1')
+	.expectStatus(405)
+	.toss()
